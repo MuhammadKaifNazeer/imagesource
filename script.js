fe55c83b844df8e50fe697614a9efb70c0a07f26
Muhammad Kaif Nazeer <muhammadkaifnazeer99@gmail.com>
2024-04-07 11:48:34 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,237 @@
+export const personalData = {
+    spanName: "Muhammad ",
+    name: " Kaif Nazeer",
+    roles: [
+      "Full stack developer",
+      "Mern stack developer",
+      "Wordpress developer",
+    ],
+    desc: "Passionate MERN stack and Wordpress web developer ready to turn your ideas into digital reality. As a Web developer, I don't just code – I solve problems and build solutions that drive real value.",
+    github: "https://github.com/MuhammadKaifNazeer",
+    linkedin: "https://www.linkedin.com/in/muhammadkaifnazeer/",
+    discordServer: "https://discord.gg/yZpeqXgqNc",
+    instagram: "https://www.instagram.com/muahammad_kaif_nazeer/",
+    email: "muhammadkaifnazeer99@gmail.com",
+  };
+  
+  export const servicesData = [
+    {
+      title: "Custom Web Application Development",
+      image: "./images/services-images/custom-web-service.png",
+    },
+    {
+      title: "Website Maintenance and Support",
+      image:
+        "./images/services-images/website-mantainace-and-support-service.png",
+    },
+    {
+      title: "Interactive Single Page Applications (SPAs)",
+      image: "./images/services-images/spa-service.png",
+    },
+    {
+      title: "UI/UX Design",
+      image: "./images/services-images/ui-ux-service.png",
+    },
+    {
+      title: "Responsive Web Design and Optimization",
+      image: "./images/services-images/responsive-web-service.jpg",
+    },
+  ];
+  
+  export const projectsData = [
+    {
+      id: 1,
+      title: "Samza",
+      categories: ["Vite Js", "AI"],
+      imageUrl:
+        "https://github.com/MuhammadKaifNazeer/samza/blob/main/public/ScreenShots/samza.png?raw=true",
+      description:
+        "Samza is an AI article summarizer, efficiently transforming extensive content into concise summaries. Through advanced techniques, it distills key insights, enhancing the reading experience for quick access to relevant information and more.",
+      url: "https://samza.netlify.app/",
+      codeLink: "https://github.com/MuhammadKaifNazeer/samza",
+    },
+    {
+      id: 2,
+      title: "GIF Mark",
+      categories: ["API"],
+      imageUrl:
+        "https://raw.githubusercontent.com/MuhammadKaifNazeer/gif_mark/main/ScreenShots/Screenshot.png",
+      description:
+        "Gif Mark is a web application that allows you to search and explore a vast collection of GIFs using the Giphy API. Gif Mark provides a visually appealing and interactive experience for discovering and sharing your favorite GIFs.",
+      url: "https://gifmark.netlify.app/",
+      codeLink: "https://github.com/MuhammadKaifNazeer/gif_mark",
+    },
+    {
+      id: 3,
+      title: "Teravarse",
+      categories: ["Crud App"],
+      imageUrl:
+        "https://raw.githubusercontent.com/MuhammadKaifNazeer/teravarse/main/screenshots/teravarse.png",
+      description:
+        "Empower your day with Teravarse, a user-friendly task manager. Effortlessly add, edit, and complete tasks for heightened productivity. Stay organized with filter options, and easily locate specific items using the robust search functionality.",
+      url: "https://teravarse.netlify.app/",
+      codeLink: "https://github.com/MuhammadKaifNazeer/teravarse",
+    },
+    {
+      id: 4,
+      title: "Sonic Voice",
+      categories: ["HTML", "CSS", "Javascript", "AI"],
+      imageUrl:
+        "https://raw.githubusercontent.com/MuhammadKaifNazeer/sonicvoice/main/screenshots/screenshot.png",
+      description:
+        "Sonic Voice is a user-friendly and efficient text-to-speech converter that transforms written text into clear and natural-sounding speech. With a sleek and intuitive interface, Sonic Voice empowers users to convert any text effortlessly into spoken words.",
+      url: "https://muhammadkaifnazeer.github.io/sonicvoice/",
+      codeLink: "https://github.com/MuhammadKaifNazeer/sonicvoice",
+    },
+    {
+      id: 5,
+      title: "Node Color Picker",
+      categories: ["Browser Extension"],
+      imageUrl:
+        "https://raw.githubusercontent.com/MuhammadKaifNazeer/node-color-picker-chrome-extension/main/screenshots/node-color-picker.png",
+      description:
+        "Node Color Picker is the ultimate, with user-friendly, secure color picker. Easily pick any color on the screen, review history, copy and clear with a single click. (Note: The extension isn't published, but you can clone the Git repo and add it locally to your browser)",
+      url: "",
+      codeLink:
+        "https://github.com/MuhammadKaifNazeer/node-color-picker-chrome-extension",
+    },
+    {
+      id: 6,
+      title: "Quick Image Editor",
+      categories: ["Javascript"],
+      imageUrl:
+        "https://raw.githubusercontent.com/MuhammadKaifNazeer/quickimageeditor/main/screenshots/Screenshot.png",
+      description:
+        "Quick Image Editor is your efficient image enhancement tool, designed for easy editing. Adjust brightness, saturation, grayscale, invert, flip, and rotate effortlessly. Simplify your editing experience with this versatile and user-friendly application",
+      url: "https://muhammadkaifnazeer.github.io/quickimageeditor/",
+      codeLink: "https://github.com/MuhammadKaifNazeer/quickimageeditor",
+    },
+    {
+      id: 7,
+      title: "Hangman",
+      categories: ["Game", "Javascript"],
+      imageUrl:
+        "https://raw.githubusercontent.com/MuhammadKaifNazeer/hangman/main/screenshots/Screenshot.png",
+      description:
+        "This hangman game is a simple yet challenging game that can be enjoyed by people of all ages. It is created using HTML, CSS, and JavaScript, and it uses a random word generator to select a word for the player to guess. With an intuitive interface.",
+      url: "https://muhammadkaifnazeer.github.io/hangman/",
+      codeLink: "https://github.com/MuhammadKaifNazeer/hangman",
+    },
+    {
+      id: 8,
+      title: "KeyStone",
+      categories: ["HTML", "CSS", "Javascript", "AI"],
+      imageUrl:
+        "https://github.com/MuhammadKaifNazeer/keystone/blob/main/sceenshots/Screenshot.png?raw=trueg",
+      description:
+        "Keystone Password Generator: Crafted with HTML, CSS, and JS, Keystone simplifies secure password creation. Generate strong, customizable passwords effortlessly for enhanced online security. A user-friendly solution for robust password.",
+      url: "https://keystonepasswordgenerator.netlify.app/",
+      codeLink: "https://github.com/MuhammadKaifNazeer/keystone/tree/main",
+    },
+    {
+      id: 9,
+      title: "Flappy Bird",
+      categories: ["Game"],
+      imageUrl:
+        "https://raw.githubusercontent.com/MuhammadKaifNazeer/flappy-bird/main/screenshots/Screenshot.png",
+      description:
+        "Flappy Bird is a challenging web-based game. Navigate a bird through an endless series of pipes.",
+      url: "https://muhammadkaifnazeer.github.io/flappy-bird/",
+      codeLink: "https://github.com/MuhammadKaifNazeer/flappy-bird",
+    },
+    {
+      id: 10,
+      title: "Brushstroke",
+      categories: ["HTML", "CSS", "Javascript"],
+      imageUrl:
+        "https://raw.githubusercontent.com/MuhammadKaifNazeer/brushstroke/main/screenshots/Screenshot.png",
+      description:
+        "A web-based drawing app that lets you create stunning digital art with ease. ",
+      url: "https://muhammadkaifnazeer.github.io/brushstroke/",
+      codeLink: "https://github.com/MuhammadKaifNazeer/brushstroke",
+    },
+  ];
+  
+  export const skillsData = [
+    {
+      name: "JavaScript",
+      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+    },
+    {
+      name: "React Js",
+      logo: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+    },
+    {
+      name: "Next Js",
+      logo: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+    },
+    {
+      name: "Vite Js",
+      logo: "https://vitejs.dev/logo.svg",
+    },
+    {
+      name: "Node Js",
+      logo: "https://nodejs.org/static/images/logo.svg",
+    },
+    {
+      name: "MongoDB",
+      logo: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+    },
+    {
+      name: "Express Js",
+      logo: "https://expressjs.com/images/favicon.png",
+    },
+    {
+      name: "Auth.js",
+      logo: "https://authjs.dev/img/logo/logo-sm.webp",
+    },
+    {
+      name: "Tailwind CSS",
+      logo: "https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg",
+    },
+    {
+      name: "Framer Motion",
+      logo: "https://framerusercontent.com/images/48ha9ZR9oZQGQ6gZ8YUfElP3T0A.png",
+    },
+    {
+      name: "Shadcn UI",
+      logo: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAIAAAD9b0jDAAAAUElEQVR4Ae3RAQbAQAxE0QXYM4e5Ro7zrxQUgJRCoFCiaPMO8A2zxnhKErAamVlmuvsUf1vcewPNG49LczEiPlmU1FUswO3XVewC1NcvG+MEvqR7x4wP3WYAAAAASUVORK5CYII=",
+    },
+    {
+      name: "Aceternity UI",
+      logo: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAQlBMVEVHcEwAAAAAAAAAAAAAAAAAAAAAAAAAAAAzMzN4eHj////w8PC5ubkaGhrY2NihoaHk5OSOjo5KSkrKyspgYGBnZ2dSxrcsAAAACHRSTlMAR6/t/w3U1ct1CuEAAACtSURBVHgBfdNFAsMgEEDRCRncI/e/ajtQxf72RVCgNrbjXzvboMYFDpK8GE4ilTMUABtO24BhSakeGdRxamOs074ZM2DJmVKI6ldfqIJ5lXKL1BHfD8QeKX0WtVeHtWyJdY81TTpGyhpzzTAaY9QYr/Q0hyO8y3xO7NHXoZqMNegmaZzHBm8SKj6pwaNK0Pgb7B9MUbVbxrB0Z9Vv9vqYgJiZXB9NUjkiQba6Dg/Gmg+3+LO+dQAAAABJRU5ErkJggg==",
+    },
+    {
+      name: "HTML",
+      logo: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+    },
+    {
+      name: "CSS",
+      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+    },
+    {
+      name: "Bootstrap",
+      logo: "https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg",
+    },
+    {
+      name: "Git",
+      logo: "https://git-scm.com/images/logos/logomark-orange@2x.png",
+    },
+    {
+      name: "Netlify",
+      logo: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcBAMAAACAI8KnAAAALVBMVEX////f9PQAt7QAu7iM2dj7/v43xMKe3934/f3a8vLGz8+Mn54hwL0ANDI9xcM3IX9gAAAAUUlEQVR4AWOgHDAKoHCZFFC4zAa4uSwBKFynVBS9YeocDUhc1ko0e1fvgrF4jI2NGvbe5lA2Nj6AxkUoRjMKt0VOT5kU0B1J2EcI76MHDsUAANZSEj24Z6GKAAAAAElFTkSuQmCC",
+    },
+    {
+      name: "Vercel",
+      logo: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAP1BMVEVHcEwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABZWVmurq7k5OT///8fHx9/f3/Hx8dQUFClpaXw8PAZrPnZAAAAC3RSTlMAOYbD7P5i/18Z3q98n4wAAACnSURBVHgBfZPFAcQwDAQNctZhcv+tHt8qqPkExOSID1FSkhi8O1IJiOSdqG6wo6lV5nHCGzJKa1zy9dzgkuYty1DaDkr1EgqUfhhBZJ/NNAzzLqcAZXixgAQXQda3sAeJGhLj8GGCBk18n7/Cwh9Jhe3wY1WhaBk/WI4woYkylhNZispYTnCeZSg9ByO4Qd4LghuyNTJz2Naa2AtmrybJAiIVfxvn8AQZnxO8pnvbuwAAAABJRU5ErkJggg==",
+    },
+    {
+      name: "Wordpress",
+      logo: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAY1BMVEUejL7///8AhrsAhLoAgrkAgbkJiLwskMAAg7mVwNoWir2y0OPI3eulyd+81uc3lMJKm8aDttXY5/F+tNN3sNGrzOGMu9fr8/jP4u7c6vLD2+m71udYoclgpctvrM/0+PsAerVrJqT6AAABVUlEQVQ4jc1Sy4KDIAxkCIQFRKytorba/v9Xbuiha9vteTfemDiPJEr9swqBiTSHT7jmY2yaeAj0K8yuQWlTagumrN/xrxFLNppZG3eGN294wuXxSBnRvvCPcDqowPVT1AP+WcVhvBxXHrd1PW16hVTeh9GLvCwbD1egH1nVhmmXJTAaIaXqFI1RFIEBbtdwLB1QrExKfuUQNDDOwvSwGNubkHdBkTAduEpehuXHJjXJJCCx4tNdnAfcfP/UoPNdQ4s6dKDmbPYNOrbazFXDXmPVMLjQXoIPxciokGidxO1kOhia/Y9JMe9qgHJrT7YAZoia9zEVTWeiCXBXYnF7Kp0Z5qd9ZXR6E/KFZOqYi8019H7WHspKgI3Vl7hNBunlJMwZeQDsfbHo6sBfynrZR7RyMJUpveGikieU6P0wo+9+OTkJS25s+37xzny8a9ZS/An9s/oGjFsQPNGp2OoAAAAASUVORK5CYII=",
+    },
+    {
+      name: "Elementor",
+      logo: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAOVBMVEWSADv/e+WQADX/f+ysL2yOADLBPomRADmXCUPvas2uMW+QADeYEUedF0+pKGX/g/P0bdO6NX7iWriS2pIeAAABVElEQVR4nO3cwW6CQBSGUQoqilZs3/9hmzbtdMEshpCBXDhnfTdfXPknTNMAAAAAAAAAAAAAAAAAR9Jm9LnDe+6y2NpZ/x7DaWJ4zyU+p4flhsfqZb/aYewmxst5cthfX9PDcuOw1a/Ynrq3iS5beJselutOCmtRmChUqLAahYlChQqrUZgoVKiwGoXJAVaM5uOyRHbcWkNxYdO35yW2CpxRGJXC+BTGpzA+hfEpjE9hfArjUxjfnMJ+ibXDkhmF10VWL/tTXnj9vC3wet7Xj/sxZy/NXBYLsZfufxFWqHCDum8KE4UKFVajMFGoUGE1ChOFuyjc+X/8/e80B9jaDrCXBqUwPoXxKYxPYXwK41MYn8L4FMbnu6dk/9+uHeD7wyMswgoVbkNholChwmoUJgoVKqxGYaJQ4XaFu38Lev/veR/gTXYAAAAAAAAAAAAAAABgA18TSj7tG0bVvwAAAABJRU5ErkJggg==",
+    },
+  ];
+  
